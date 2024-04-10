295171b31e7d44535848012f251adeb756068266
Guilherme Abreu <guilherme.abreu@microware.com.br>
2024-04-10 17:59:54 -0300

    testando soluçoes

--- js/mask.js
+++ js/mask.js
@@ -1,11 +1,105 @@
-let inputValue = "";
-let mask = "";
+let allMasks = [
+  { cpf: "999.999.999-99" },
+  { cell: "(99)9 9999-9999" },
+  { date: "99/99/9999" },
+  { cep: "9999-999" },
+  { cnpj: "99.999.999/9999-99" },
+  { datetime: "99/99/9999 99:99" },
+  { time: "99:99" },
+  { card: "9999 9999 9999 9999" },
+  { expiration: "99/99" },
+  { agency: "9999" },
+  { account: "99999 9" },
+];
 
-function blockNotNumeric(event) {
-  mask = document.getElementById("inputValue").placeholder;
+let inputFocusValue = "";
+let finalInputValue = "";
+
+function findMaskByType(type) {
+  const foundObj = allMasks.find((obj) => obj[type]);
+  if (foundObj) {
+    return foundObj[type];
+  } else {
+    return null;
+  }
+}
+
+const inputs = document.querySelectorAll("input.mask");
+for (let i = 0; i < inputs.length; i++) {
+  var typeMask = inputs[i].className.split(" ")[1];
+  let mask = findMaskByType(typeMask);
+  // inputs[i].addEventListener("keypress", (event) =>
+  //   blockNotNumeric(event, mask)
+  // );
+  inputs[i].addEventListener("focus", () => handleFocus(inputs[i], mask));
+  inputs[i].addEventListener("click", () => handleClick(inputs[i]));
+  inputs[i].addEventListener("input", (event) => {
+    const valorDigitado = event.target.value;
+    // if (!/^[0-9]+$/.test(valorDigitado)) {
+    //   return;
+    // }
+    const novoFormatoCpf = formatarCpf(valorDigitado);
+    // console.log(novoFormatoCpf);
+    // inputs[i].value = novoFormatoCpf;
+  });
+  // inputs[i].addEventListener("blur", () => handleMask(inputs[i], mask));
+}
+
+function formatarCpf(valor) {
+  console.log(valor);
+  let novoValor = "";
+  let indiceMascara = 0;
+console.log(inputFocusValue)
+  for (let i = 0; i < valor.length; i++) {
+    if (inputFocusValue[indiceMascara] !== "_") {
+      novoValor += inputFocusValue[indiceMascara];
+      indiceMascara++;
+    }
+
+    novoValor += valor[i];
+  }
+  console.log(novoValor);
+
+  return novoValor;
+}
+
+function handleFocus(input, mask) {
+  input.value = mask.replaceAll("9", "_");
+  inputFocusValue = input.value;
+}
+
+function handleClick(input) {
+  input.selectionStart = 0;
+  input.selectionEnd = 0;
+}
+
+function handleInput(event, mask) {
+  // let input = event.target;
+  // let inputValue = input.value;
+  // let maskedValue = mask.replace(/9/g, "_");
+  // let finalValue = "";
+  // let j = 0;
+  // let cursorPosition = input.selectionStart;
+  // for (let i = 0; i < mask.length; i++) {
+  //    if (mask[i] === "9" && j < inputValue.length) {
+  //      finalValue += inputValue[j++];
+  //    } else {
+  //      finalValue += mask[i];
+  //    }
+  // }
+  // console.log(finalValue);
+  // if (cursorPosition < inputValue.length) {
+  //    input.selectionStart = cursorPosition;
+  //    input.selectionEnd = cursorPosition;
+  // } else {
+  //    input.selectionStart = finalValue.length;
+  //    input.selectionEnd = finalValue.length;
+  // }
+}
+
+function blockNotNumeric(event, mask) {
   let countNines = mask.split("").filter((char) => char === "9").length;
   const inputElement = event.target.value + String.fromCharCode(event.keyCode);
-  console.log(inputElement);
   const inputChar = String.fromCharCode(event.keyCode);
   if (!/^\d+$/.test(inputChar)) {
     event.preventDefault();
@@ -15,12 +109,8 @@ function blockNotNumeric(event) {
   }
 }
 
-function handleMask(mask) {
-  inputValue = document.getElementById("inputValue").value;
-  let value = inputValue.replace(/\D/g, "");
-  let inputElement = document.getElementById("inputValue");
-  inputElement.placeholder = mask;
-  console.log(mask);
+function handleMask(input, mask) {
+  let value = input.value.replace(/\D/g, "");
   let maskedValue = "";
   let j = 0;
   for (let i = 0; i < mask.length; i++) {
@@ -32,7 +122,7 @@ function handleMask(mask) {
       maskedValue += mask[i];
     }
   }
-  if (inputValue !== "") {
-    document.getElementById("inputValue").value = maskedValue;
+  if (input.value !== "") {
+    input.value = maskedValue;
   }
 }
