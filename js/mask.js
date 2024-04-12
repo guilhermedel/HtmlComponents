19d801c7c153c97b0f61c8a90d4a9169d006d621
guilhermedel <glhrmgithub@gmail.com>
2024-04-12 01:47:51 -0300

    feat: finished validations inputs components

--- js/mask.js
+++ js/mask.js
@@ -10,50 +10,75 @@ let allMasks = [
   { expiration: "99/99" },
   { agency: "9999" },
   { account: "99999 9" },
-];
-
-let inputFocusValue = "";
-let finalInputValue = "";
-
-function findMaskByType(type) {
+ ];
+ 
+ function findMaskByType(type) {
   const foundObj = allMasks.find((obj) => obj[type]);
-  if (foundObj) {
-    return foundObj[type];
-  } else {
-    return null;
+  return foundObj ? foundObj[type] : null;
+ }
+
+ function applyMaskOnInputChange(event, formattedMask) {
+  const currentValue = event.target.value;
+  if (currentValue !== formattedMask) {
+      event.target.value = formattedMask;
   }
 }
-
-const inputs = document.querySelectorAll("input.mask");
-for (let i = 0; i < inputs.length; i++) {
-  var typeMask = inputs[i].className.split(" ")[1];
-  console.log("mask: " + typeMask)
-  let mask = findMaskByType(typeMask);
-  let formattedMask = mask.replaceAll("9", "_");
-  inputs[i].addEventListener("keypress", (event) =>
-    handleInput(event, inputs[i])
-  );
-  inputs[i].addEventListener("focus", () =>
-    handleFocus(formattedMask, inputs[i])
-  );
-}
-
-function handleFocus(formattedMask, input) {
-  input.value = formattedMask;
-}
-function handleInput(event, input) {
-  input.selectionStart = 0;
-  input.selectionEnd = 0;
-  let currentValue = input.value;
-  let enteredValue = String.fromCharCode(event.which || event.keyCode);
+ 
+ function initializeInputs() {
+  const inputs = document.querySelectorAll("input.mask");
+  inputs.forEach(input => {
+     const typeMask = input.className.split(" ")[1];
+     const mask = findMaskByType(typeMask);
+     const formattedMask = mask.replaceAll("9", "_");
+     input.value = formattedMask;
+     input.addEventListener("keydown", handleKeyDown);
+     input.addEventListener("keyup", handleKeyUp);
+     input.addEventListener("focus", () => handleFocus(formattedMask, input));
+     input.addEventListener("input", (event) => applyMaskOnInputChange(event, formattedMask));
+  });
+ }
+ 
+ function handleKeyDown(event) {
+  const input = event.target;
+  input.dataset.cursorPosition = input.selectionStart;
+  const enteredValue = String.fromCharCode(event.which || event.keyCode);
   if (/^\d$/.test(enteredValue)) {
-    let nextIndex = currentValue.indexOf("_");
-    if (nextIndex !== -1) {
-      input.value =
-        currentValue.substring(0, nextIndex) +
-        enteredValue +
-        currentValue.substring(nextIndex + 1);
-    }
+     const currentValue = input.value;
+     const nextIndex = currentValue.indexOf("_");
+     if (nextIndex !== -1) {
+       input.value = currentValue.substring(0, nextIndex) + enteredValue + currentValue.substring(nextIndex + 1);
+       const cursorPosition = parseInt(input.dataset.cursorPosition, 10);
+       input.selectionStart = cursorPosition + 1;
+       input.selectionEnd = cursorPosition + 1;
+     }
   }
-  event.preventDefault();
-}
+  if (event.key !== "ArrowLeft" && event.key !== "ArrowRight") {
+     event.preventDefault();
+  }
+ }
+ 
+ function handleKeyUp(event) {
+  const input = event.target;
+  if (event.key === "Backspace") {
+     const currentValue = input.value;
+     const cursorPosition = parseInt(input.dataset.cursorPosition, 10);
+     if (cursorPosition > 0) {
+       const charBeforeCursor = currentValue.charAt(cursorPosition - 1);
+       if (/^\d$/.test(charBeforeCursor) || charBeforeCursor === "_") {
+         input.value = currentValue.substring(0, cursorPosition - 1) + "_" + currentValue.substring(cursorPosition);
+         input.selectionStart = cursorPosition - 1;
+         input.selectionEnd = cursorPosition - 1;
+       } else {
+         event.preventDefault();
+       }
+     }
+  }
+ }
+ 
+ function handleFocus(formattedMask, input) {
+     if (input.value === '') {
+         input.value = formattedMask;
+     }
+ }
+ document.addEventListener("DOMContentLoaded", initializeInputs);
+ 
